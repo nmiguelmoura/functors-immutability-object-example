46885e2b9bb622ac42350c7c75b0f2daab1b9e47
nmiguelmoura <nmiguelmoura@gmail.com>
2020-08-15 17:10:27 +0100

    Add getValue method

--- src/BaseObject.js
+++ src/BaseObject.js
@@ -6,6 +6,10 @@ BaseObject.prototype.isValid = function () {
   return this.value instanceof Some;
 };
 
+BaseObject.prototype.getValue = function () {
+  return this.isValid() ? this.value.value : null;
+}
+
 BaseObject.prototype.getErrorMessage = function () {
   return this.isValid() ? "" : this.value.value.message;
 };
--- src/index.js
+++ src/index.js
@@ -10,32 +10,36 @@ if (!myPhoneNumber.isValid()) {
   console.log(`WILL THROW: ${myPhoneNumber.getErrorMessage()}`);
 }
 
+// ********** GET VALUE ****************
+// // SCENARIO 1 - get the stored value;
+// console.log(myPhoneNumber.getValue());
+
 // ********** USE PROTOTYPE ****************
-// // SCENARIO 1 - call prototype function
+// // SCENARIO 2 - call prototype function
 // console.log(myPhoneNumber.fullSentence());
 
 // ********** USE COMPOSITION ****************
-// // SCENARIO 2 - map once
+// // SCENARIO 3 - map once
 // const { value: completePhone } = myPhoneNumber.value.map(
 //   (val) => `${val.ind}${val.numb}`
 // );
 // console.log(completePhone);
 
-// // SCENARIO 3 - chain maps
+// // SCENARIO 4 - chain map
 // const { value: fullSentence } = myPhoneNumber.value
 //   .map((val) => `${val.ind}${val.numb}`)
 //   .map((val) => `The complete phone number is ${val}`);
 // console.log(fullSentence);
 
-// // SCENARIO 4 - somewhere you get a null value
-// const { value: anotherFullSentence } = myPhoneNumber.value
+// // SCENARIO 5 - somewhere you get a null value
+// const anotherFullSentence = myPhoneNumber.value
 //   .map((val) => `${val.ind}${val.numb}`)
 //   .map(() => null)
 //   .map((val) => `The complete phone number is ${val}`);
 // console.log(anotherFullSentence);
 
 // ********** IMMUTABILITY ****************
-// // SCENARIO 5 - its immutable
+// // SCENARIO 6 - its immutable
 // console.log(myPhoneNumber.value);
 // myPhoneNumber.value.value.numb = "123";
 // console.log(myPhoneNumber.value);
@@ -46,15 +50,23 @@ if (!myPhoneNumber.isValid()) {
 // myPhoneNumber.test = "123";
 // console.log(myPhoneNumber.value);
 
+// SCENARIO 7 - getValue also returns immutable
+// const value = myPhoneNumber.getValue();
+// console.log(value);
+// value.numb = "123";
+// value.ind = "456";
+// value.test = "hello";
+// console.log(value);
+
 // ********** COMPARE OBJECTS ****************
-// SCENARIO 6 - objects with same value
+// SCENARIO 8 - objects with same value
 // const secondPhoneNumber = PhoneNumber.of({
 //   numb: "123456789",
 //   ind: "+351",
 // });
 // console.log(myPhoneNumber.equals(secondPhoneNumber));
 
-// SCENARIO 7 - objects with same value
+// SCENARIO 9 - objects with same value
 // const thirdPhoneNumber = PhoneNumber.of({
 //   numb: "111111111",
 //   ind: "+351",
