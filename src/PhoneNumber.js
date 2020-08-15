386b46a7354b75a9091cb815875cac445fb0a548
nmiguelmoura <nmiguelmoura@gmail.com>
2020-08-15 15:16:32 +0100

    v2.0

--- src/PhoneNumber.js
+++ src/PhoneNumber.js
@@ -1,11 +1,22 @@
 const { Some } = require("./Functors");
+const deepFreeze = require("./deepFreeze");
 const { isString, isAllDigits, isFromPortugal } = require("./validators");
 
 const PhoneNumber = function (value) {
-  return Some.of(value)
+  this.value = Some.of(value)
     .validate((val) => isString(val.numb))
     .validate((val) => isAllDigits(val.numb))
     .validate((val) => isFromPortugal(val.ind));
+};
+
+PhoneNumber.of = function (value) {
+  return deepFreeze(new PhoneNumber(value));
+};
+
+PhoneNumber.prototype.fullSentence = function () {
+  return this.value
+    .map((val) => `${val.ind}${val.numb}`)
+    .map((val) => `The complete phone number is ${val}`);
 };
 
 module.exports = PhoneNumber;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const PhoneNumber = require("./PhoneNumber");
 const { Nothing } = require("./Functors");
 
-const myPhoneNumber = PhoneNumber({
+const myPhoneNumber = PhoneNumber.of({
   numb: "123456789",
   ind: "+351",
 });
@@ -12,19 +12,19 @@ if (myPhoneNumber instanceof Nothing) {
 }
 
 // // SCENARIO 1 - map once
-// const { value: completePhone } = myPhoneNumber.map(
+// const { value: completePhone } = myPhoneNumber.value.map(
 //   (val) => `${val.ind}${val.numb}`
 // );
 // console.log(completePhone);
 
 // // SCENARIO 2 - chain maps
-// const { value: fullSentence } = myPhoneNumber
+// const { value: fullSentence } = myPhoneNumber.value
 //   .map((val) => `${val.ind}${val.numb}`)
 //   .map((val) => `The complete phone number is ${val}`);
 // console.log(fullSentence);
 
 // // SCENARIO 3 - somewhere you get a null value
-// const { value: anotherFullSentence } = myPhoneNumber
+// const { value: anotherFullSentence } = myPhoneNumber.value
 //   .map((val) => `${val.ind}${val.numb}`)
 //   .map(() => null)
 //   .map((val) => `The complete phone number is ${val}`);
@@ -32,15 +32,20 @@ if (myPhoneNumber instanceof Nothing) {
 
 // // SCENARIO 4 - its immutable
 // console.log(myPhoneNumber.value);
-// myPhoneNumber.value.numb = "123";
+// myPhoneNumber.value.value.numb = "123";
+// console.log(myPhoneNumber.value);
+// myPhoneNumber.value.value = "123";
 // console.log(myPhoneNumber.value);
 // myPhoneNumber.value = "123";
 // console.log(myPhoneNumber.value);
 
 // // SCENARIO 5 - results of map also immutable
-// const completePhoneObject = myPhoneNumber.map(
+// const completePhoneObject = myPhoneNumber.value.map(
 //   (val) => `${val.ind}${val.numb}`
 // );
 // console.log(completePhoneObject);
 // completePhoneObject.value = "123";
 // console.log(completePhoneObject);
+
+// // SCENARIO 6 - call prototype function
+// console.log(myPhoneNumber.fullSentence());
